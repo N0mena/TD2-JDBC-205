494eba114f43aa471045d43d13f077980330f554
N0mena <alida.rajaonarivelo@gmail.com>
2026-01-03 21:43:14 +0300

    fix: findIngredients

--- src/main/java/school/hei/DataRetriever.java
+++ src/main/java/school/hei/DataRetriever.java
@@ -12,12 +12,12 @@ public class DataRetriever {
 
     public Dish findDishById(Integer id) {
 
-        Dish dishById = null;
+        Dish dishById ;
         List<Ingredient> ingredients = new ArrayList<>();
         Connection databaseConnection = dbConnection.getConnection();
 
-        String sqlDish = "SELECT id, name, dish_type FROM dish WHERE id = ?";
-        String sqlIngredient =  "SELECT id, name FROM ingredient WHERE id_dish = ?";
+        String sqlDish = "SELECT d.id, d.name, d.dish_type FROM dish d WHERE d.id = ?";
+        String sqlIngredient =  "SELECT id, name , price, category , id_dish FROM ingredient WHERE id_dish = ?";
 
         try (
              PreparedStatement preparedStatement = databaseConnection.prepareStatement(sqlDish)) {
@@ -25,14 +25,16 @@ public class DataRetriever {
             preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            while (resultSet.next()) {
-                int dishId = resultSet.getInt("id");
-                String dishName = resultSet.getString("name");
-                String dishType = resultSet.getString("dish_type");
+            if (resultSet.next()) {
 
+                    int dishId = resultSet.getInt(1);
+                    String dishName = resultSet.getString("name");
+                    String dishType = resultSet.getString("dish_type");
 
-                dishById = new Dish(dishId, dishName, dishType);
+                     dishById = new Dish(dishId, dishName, dishType);
 
+            } else {
+                return null;
             }
 
         } catch (SQLException e) {
@@ -47,8 +49,11 @@ public class DataRetriever {
 
 ;              int ingredientId = resultSet.getInt(1);
                String ingredientName = resultSet.getString("name");
+               Double ingredientPrice = resultSet.getDouble(3);
+               String ingredientCategory = resultSet.getString(4);
+               int dishId = resultSet.getInt(5);
 
-                Ingredient dishIngredient = new Ingredient(ingredientId, ingredientName);
+                Ingredient dishIngredient = new Ingredient(ingredientId, ingredientName, ingredientPrice, ingredientCategory, dishId);
                 ingredients.add(dishIngredient);
             }
 
@@ -56,9 +61,7 @@ public class DataRetriever {
             throw new RuntimeException(e);
         }
 
-        if(dishById != null){
         dishById.setIngredients(ingredients);
-        }
 
         return dishById;
     }
@@ -69,26 +72,46 @@ public class DataRetriever {
 
 
     public List<Ingredient> findIngredients(int page, int size){
-        List<Ingredient> ingredients = new ArrayList<>();
+        List<Ingredient> ingredientsList = new ArrayList<>();
         Connection databaseConnection = dbConnection.getConnection();
 
         try{
+
             int offset = (page - 1) * size;
-            String sql_query = "select i.id , i.name from ingredient i order by id limit ? offset ? ";
+            String sql_query = "SELECT id, name , price, category , id_dish from ingredient order by id limit ? offset ? ";
             PreparedStatement st = databaseConnection.prepareStatement(sql_query);
+
+            st.setInt(1, page);
+            st.setInt(2, offset);
+
             ResultSet rs = st.executeQuery();
+
+
+
             while(rs.next()){
 
-                rs.getInt("id");
-                rs.getString("name");
+                int ingredientId = rs.getInt("id");
+                String ingredientName = rs.getString("name");
+                Double ingredientPrice = rs.getDouble(3);
+                String ingredientCategory = rs.getString(4);
+                int dishId = rs.getInt(5);
+
+
+                Ingredient ing =  new Ingredient(ingredientId, ingredientName, ingredientPrice, ingredientCategory, dishId);
+
+                ingredientsList.add(ing);
 
             }
         }catch (SQLException e){
             throw new RuntimeException(e);
         }
 
-        return ingredients;
+        return ingredientsList;
     }
+
+
+
+
 
     public List<Ingredient> createIngredients (List<Ingredient> newIngredients){
         List<Ingredient> ingredientsCreated = new ArrayList<>();
