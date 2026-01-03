e4e01ec3c12f3be471254ad1b11ceee1285b0b28
N0mena <alida.rajaonarivelo@gmail.com>
2026-01-03 16:53:02 +0300

    fix: findDishByID() - still to fix later

--- src/main/java/school/hei/DataRetriever.java
+++ src/main/java/school/hei/DataRetriever.java
@@ -10,25 +10,63 @@ import java.util.List;
 public class DataRetriever {
     private final DBConnection dbConnection = new DBConnection();
 
-    public Dish findDishById(Integer id){
-        Dish dishById = new Dish();
+    public Dish findDishById(Integer id) {
+
+        Dish dishById = null;
+        List<Ingredient> ingredients = new ArrayList<>();
         Connection databaseConnection = dbConnection.getConnection();
 
-        try{
-            PreparedStatement preparedStatement = databaseConnection.prepareStatement("SELECT d.is, d.name, d.dish_type FROM dish d WHERE id = ?");
+        String sqlDish = "SELECT id, name, dish_type FROM dish WHERE id = ?";
+        String sqlIngredient =  "SELECT id, name FROM ingredient WHERE id_dish = ?";
+
+        try (
+             PreparedStatement preparedStatement = databaseConnection.prepareStatement(sqlDish)) {
+
+            preparedStatement.setInt(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
-            while(resultSet.next()){
-                dishById.setId(resultSet.getInt("id"));
-                dishById.setName(resultSet.getString("name"));
-                dishById.setDishType(DishTypeEnum.valueOf(resultSet.getString("dish_type")));
+
+            while (resultSet.next()) {
+                int dishId = resultSet.getInt("id");
+                String dishName = resultSet.getString("name");
+                String dishType = resultSet.getString("dish_type");
+
+
+                dishById = new Dish(dishId, dishName, dishType);
+
             }
 
-        } catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
+        }
+
+        try{
+            PreparedStatement preparedStatement = databaseConnection.prepareStatement(sqlIngredient);
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+
+;              int ingredientId = resultSet.getInt(1);
+               String ingredientName = resultSet.getString("name");
+
+                Ingredient dishIngredient = new Ingredient(ingredientId, ingredientName);
+                ingredients.add(dishIngredient);
+            }
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+
+        if(dishById != null){
+        dishById.setIngredients(ingredients);
         }
 
         return dishById;
     }
+
+
+
+
+
 
     public List<Ingredient> findIngredients(int page, int size){
         List<Ingredient> ingredients = new ArrayList<>();
@@ -40,12 +78,38 @@ public class DataRetriever {
             PreparedStatement st = databaseConnection.prepareStatement(sql_query);
             ResultSet rs = st.executeQuery();
             while(rs.next()){
+
                 rs.getInt("id");
                 rs.getString("name");
+
             }
         }catch (SQLException e){
             throw new RuntimeException(e);
         }
+
         return ingredients;
     }
+
+    public List<Ingredient> createIngredients (List<Ingredient> newIngredients){
+        List<Ingredient> ingredientsCreated = new ArrayList<>();
+        Connection databaseConnection = dbConnection.getConnection();
+
+
+        return ingredientsCreated;
+    }
+
+    public Dish saveDish(Dish dishToSave){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    public List<Dish> findDishsByIngredientName (String ingredientName){
+        throw new RuntimeException("not yet implemented");
+    }
+
+    public List<Ingredient> findIngredientsByCriteria
+            (String ingredientName, CategoryEnum category, String dishName, int page, int size){
+        throw new RuntimeException("not yet implemented");
+    }
 }
+
+
